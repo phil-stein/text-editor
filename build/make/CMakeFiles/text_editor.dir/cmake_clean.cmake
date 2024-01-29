837d8eefaa2486097d0a26390d099dc6eac83365
Phil-Laptop <philipp033.25@gmail.com>
2024-01-29 03:38:12 +0100

    LAPTOP: removing build / bin folder files

---
+++ build/make/CMakeFiles/text_editor.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "C:/Workspace/C/text_editor/bin/text_editor.exe"
+  "C:/Workspace/C/text_editor/bin/text_editor.exe.manifest"
+  "C:/Workspace/C/text_editor/bin/text_editor.pdb"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/app.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/app.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/cmd.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/cmd.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/main.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/main.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/ui.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/app/ui.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/file_io.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/file_io.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/glad.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/glad.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/input.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/input.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/renderer.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/renderer.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/shader.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/shader.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/texture.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/texture.c.obj.d"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/window.c.obj"
+  "CMakeFiles/text_editor.dir/C_/Workspace/C/text_editor/src/core/window.c.obj.d"
+  "libtext_editor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/text_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/make/CMakeFiles/text_editor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for text_editor.
+# This may be replaced when dependencies are built.
